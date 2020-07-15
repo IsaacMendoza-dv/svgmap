6022c0b78acdec1b1e601c97d9f9fc523f57d15d
IsaacMendoza-dv <ca.isaac.mendoza@gmail.com>
2020-07-15 14:11:07 -0500

    first commit

---
+++ assets/scripts/event-List.js
@@ -0,0 +1,7 @@
+// $(document).ready(function () {
+//   $(".estado ").on("click",function () {
+//     var t = $(this).attr('id');
+//     alert('Ayuda');
+//     // $(".description").html(`<p>Seleccionaste el ID </p>`)
+//   })
+// })
